bb8d064ae73d6b6ee473e15407657fb356b16008
Clyde McQueen <clyde@mcqueen.net>
2025-11-06 10:53:44 -0800

    Fix launch arg

--- orca_bringup/launch/sim.launch.py
+++ orca_bringup/launch/sim.launch.py
@@ -38,9 +38,9 @@ def generate_launch_description():
         ),
 
         DeclareLaunchArgument(
-            'base',
+            'bridge',
             default_value='True',
-            description='Launch base controller?',
+            description='Launch SLAM bridge?',
         ),
 
         DeclareLaunchArgument(
@@ -157,7 +157,7 @@ def generate_launch_description():
             PythonLaunchDescriptionSource(os.path.join(orca_bringup_dir, 'launch', 'bringup.launch.py')),
             launch_arguments={
                 'use_sim_time': 'True',
-                'base': LaunchConfiguration('base'),
+                'bridge': LaunchConfiguration('bridge'),
                 'orb': LaunchConfiguration('orb'),
                 'mav_device': 'udpin:0.0.0.0:14551',
                 'use_vpe': LaunchConfiguration('use_vpe'),
